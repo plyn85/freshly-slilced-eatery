3d46d38aa9ce793ae46709701717b272b81d63a9
plyn85 <plyn99@gmail.com>
2021-06-28 12:48:10 +0100

    refactoring success.js functions

--- public/javascript/views/success.js
+++ public/javascript/views/success.js
@@ -1,34 +1,29 @@
 // add the customer and payment info to success html page
-//customer info
+
 let getCusInfo = () => {
   //get the customer info from the local storage
   let obj = JSON.parse(window.localStorage.getItem("customer"));
-  let customerInfo;
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      customerInfo = `
-        
-        
+  let customerInfo = `
           <h2 class="text-center">Order id: ${
             obj._id
           }</h2><hr class="green hr-order">
           <h3 class="text-center">Name: ${
             obj.name
           }</h3><hr class="green hr-order">
-          <h4 class="text-center">Email: ${
-            obj.email
-          }</h4><hr class="green hr-order">
+          <h4 class="text-center">Email: ${obj.email}</h4>
+          <h4 class="text-center">Delivery Address: ${obj.address}</h4>
+          <hr class="green hr-order">
           <h4 class="text-center">Ordered on: ${obj.time_created.substring(
             0,
             10
           )}</h4>
-          <h4 class="text-center">Collection time: ${
-            obj.collection_time
+          <h4 class="text-center">
+          Collection time: ${
+            obj.collection_delivery_time
           }</h4><hr class="green hr-order">
           
           `;
-    }
-  }
+  // add the customer info to the div element
   document.getElementById("successInfo").innerHTML = customerInfo;
 };
 
@@ -36,14 +31,11 @@ let getCusInfo = () => {
 getCusInfo();
 //passed in to format amount charged as currency
 let options = { style: "currency", currency: "EUR" };
-//payment info
+//function to handle payment info
 let getPaymentInfo = () => {
   //get the payment info from the local storage
   let obj = JSON.parse(window.localStorage.getItem("chargeInfo"));
-  let PaymentInfo;
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      PaymentInfo = `
+  let paymentInfo = `
           <h4 class="text-center">invoice number: ${
             obj.invoice_num
           }</h4><hr class="green hr-order">
@@ -51,11 +43,9 @@ let getPaymentInfo = () => {
             "en-US",
             options
           ).format(obj.amount_charged / 100)} </h4><hr class="green hr-order">
-         
          `;
-    }
-  }
-  document.getElementById("paymentInfo").innerHTML = PaymentInfo;
+
+  document.getElementById("paymentInfo").innerHTML = paymentInfo;
 };
 //call the function
 getPaymentInfo();

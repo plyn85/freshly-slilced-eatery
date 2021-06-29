8b3ac82709ebd8c53a1ea41db392f9af3b9caffb
plyn85 <plyn99@gmail.com>
2021-06-29 16:45:29 +0100

    design and info display changes all info now displaying correctly

--- public/javascript/views/success.js
+++ public/javascript/views/success.js
@@ -1,51 +1,47 @@
 // add the customer and payment info to success html page
 
 let getCusInfo = () => {
-  //get the customer info from the local storage
+  //passed in to format amount charged as currency
+  let options = { style: "currency", currency: "EUR" };
+  //get the customer and payment info from the local storage
   let obj = JSON.parse(window.localStorage.getItem("customer"));
+  let chargeObj = JSON.parse(window.localStorage.getItem("chargeInfo"));
+  //add the information to customerInfo variable
   let customerInfo = `
-          <h2 class="text-center">Order id: ${
-            obj._id
-          }</h2><hr class="green hr-order">
-          <h3 class="text-center">Name: ${
-            obj.name
-          }</h3><hr class="green hr-order">
-          <h4 class="text-center">Email: ${obj.email}</h4>
-          <h4 class="text-center">Delivery Address: ${obj.address}</h4>
-          <hr class="green hr-order">
-          <h4 class="text-center">Ordered on: ${obj.time_created.substring(
-            0,
-            10
-          )}</h4>
-          <h4 class="text-center">
-          Collection time: ${
-            obj.collection_delivery_time
-          }</h4><hr class="green hr-order">
-          
-          `;
-  // add the customer info to the div element
+          <div class="col-10 col-md-8 col-xl-4 mont-font">
+          <h5>Order id: ${obj._id}</h5><hr class="green hr-order">
+          <h5>Name: ${obj.name}</h5><hr class="green hr-order">
+          <h5>Email: ${obj.email}</h5>`;
+  //check if the delivery option has been chosen
+  if (obj.delivery == "yes") {
+    customerInfo += `<h5 class="text-center">Delivery To: ${obj.address}</h5>`;
+  }
+
+  customerInfo += `<hr class="green hr-order">
+          <h5>Ordered on: ${obj.time_created.substring(0, 10)}</h5>
+          <h5>
+          <hr class="green hr-order">`;
+  //check if the collection option has been chosen
+  if (obj.collection == "yes") {
+    customerInfo += `Collection`;
+  }
+  //if it has not
+  else {
+    customerInfo += `Delivery`;
+  }
+  customerInfo += ` time: ${
+    obj.collection_delivery_time
+  }</h5><hr class="green hr-order"> <h5>invoice number: ${
+    chargeObj.invoice_num
+  }</h5><hr class="green hr-order">
+          <h5>Amount charged: ${new Intl.NumberFormat("en-US", options).format(
+            chargeObj.amount_charged / 100
+          )} </h5><hr class="green hr-order">
+          </div>
+         `;
+  // add the customer and payment info to the div element
   document.getElementById("successInfo").innerHTML = customerInfo;
 };
 
 //call the function
 getCusInfo();
-//passed in to format amount charged as currency
-let options = { style: "currency", currency: "EUR" };
-//function to handle payment info
-let getPaymentInfo = () => {
-  //get the payment info from the local storage
-  let obj = JSON.parse(window.localStorage.getItem("chargeInfo"));
-  let paymentInfo = `
-          <h4 class="text-center">invoice number: ${
-            obj.invoice_num
-          }</h4><hr class="green hr-order">
-          <h4 class="text-center">Amount charged: ${new Intl.NumberFormat(
-            "en-US",
-            options
-          ).format(obj.amount_charged / 100)} </h4><hr class="green hr-order">
-         `;
-
-  document.getElementById("paymentInfo").innerHTML = paymentInfo;
-};
-//call the function
-getPaymentInfo();

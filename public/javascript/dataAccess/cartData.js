0368ea9ac47d977c34664e5795402700c127d1a0
plyn85 <plyn99@gmail.com>
2021-08-20 18:17:18 +0100

    moving stripe functions to separte stripeData file

--- public/javascript/dataAccess/cartData.js
+++ public/javascript/dataAccess/cartData.js
@@ -92,43 +92,5 @@ let changeQuantity = async (mealData) => {
   }
 };
 
-//function to handle strip payments
-let stripePayment = async (token) => {
-  //get the user id form local storage
-  let userId = helperFunctions.getObjectFromLocalStorage("userId");
-  const url = `${api.BASE_URL}/cart/payment/${userId}`;
-  // console.log("token", token);
-  //http method
-  let httpMethod = "POST";
-  //build the request method
-  const request = api.fetchInit(httpMethod, JSON.stringify(token));
-
-  try {
-    // stripe data
-    let result = await api.getDataAsync(url, request);
-    //check the result of the API call
-    if (helperFunctions.checkFetchRequestResult(result)) {
-      //add the invoice number to local storage
-      helperFunctions.addToLocalStorageObject(
-        "customerOrder",
-        "invoice_number",
-        result.invoice_num
-      );
-      //add the amount charged to local storage
-      helperFunctions.addToLocalStorageObject(
-        "customerOrder",
-        "amount",
-        result.amount_charged
-      );
-      //reset the user id to 0
-      helperFunctions.addObjectToLocalStorage("userId", 0);
-      return true;
-    } else return false;
-  } catch (err) {
-    // catch and log any errors
-    console.log(err);
-  }
-};
-
-// End Functions
-export { getCartItems, getCart, deleteCartItem, changeQuantity, stripePayment };
+// exports
+export { getCartItems, getCart, deleteCartItem, changeQuantity };

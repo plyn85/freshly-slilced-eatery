9dd1d0714cab98b4f272b987d8e34e9d4b28aec8
plyn85 <plyn99@gmail.com>
2021-08-23 20:43:26 +0100

    style changes to success info-displaying correctly

--- public/javascript/views/success.js
+++ public/javascript/views/success.js
@@ -1,46 +1,48 @@
+//imports
+import * as helperFunctions from "../helper_functions/helpers.js";
 // add the customer and payment info to success html page
 
 let getCusInfo = () => {
   //passed in to format amount charged as currency
   let options = { style: "currency", currency: "EUR" };
   //get the customer and payment info from the local storage
-  let obj = JSON.parse(window.localStorage.getItem("customer"));
-  let chargeObj = JSON.parse(window.localStorage.getItem("chargeInfo"));
+  let obj = helperFunctions.getObjectFromLocalStorage("customerOrder");
+  console.log(obj);
   //add the information to customerInfo variable
-  let customerInfo = `
+  let customerOrderInfo = `
           <div class="col-10 col-md-8 col-xl-4 mont-font">
           <h5>Order id: ${obj._id}</h5><hr class="green hr-order">
           <h5>Name: ${obj.name}</h5><hr class="green hr-order">
-          <h5>Email: ${obj.email}</h5>`;
+          <h5>Email: ${obj.email}</h5><hr class="green hr-order">`;
   //check if the delivery option has been chosen
   if (obj.delivery == "yes") {
-    customerInfo += `<h5 class="text-center">Delivery To: ${obj.address}</h5>`;
+    customerOrderInfo += `<h5>Delivery To: ${obj.address}</h5>`;
   }
 
-  customerInfo += `<hr class="green hr-order">
+  customerOrderInfo += `<hr class="green hr-order">
           <h5>Ordered on: ${obj.time_created.substring(0, 10)}</h5>
           <h5>
           <hr class="green hr-order">`;
   //check if the collection option has been chosen
   if (obj.collection == "yes") {
-    customerInfo += `Collection`;
+    customerOrderInfo += `Collection`;
   }
   //if it has not
   else {
-    customerInfo += `Delivery`;
+    customerOrderInfo += `Delivery`;
   }
-  customerInfo += ` time: ${
+  customerOrderInfo += ` time: ${
     obj.collection_delivery_time
   }</h5><hr class="green hr-order"> <h5>invoice number: ${
-    chargeObj.invoice_num
+    obj.invoice_number
   }</h5><hr class="green hr-order">
           <h5>Amount charged: ${new Intl.NumberFormat("en-US", options).format(
-            chargeObj.amount_charged / 100
+            obj.amount
           )} </h5><hr class="green hr-order">
           </div>
          `;
   // add the customer and payment info to the div element
-  document.getElementById("successInfo").innerHTML = customerInfo;
+  document.getElementById("successInfo").innerHTML = customerOrderInfo;
 };
 
 //call the function

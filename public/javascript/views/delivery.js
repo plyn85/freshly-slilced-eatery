d82d775ee44efc0eb8f3c50edca4f217afe003e3
plyn85 <plyn99@gmail.com>
2021-08-30 14:19:11 +0100

    address being added to deilvery too box if user logged in

--- public/javascript/views/delivery.js
+++ public/javascript/views/delivery.js
@@ -1,18 +1,19 @@
 //imports
 import * as helperFunctions from "../helper_functions/helpers.js";
+import { checkStatus } from "../auth/jwtAuth.js";
+//get the user address info
+let addressInfo = document.getElementById("address");
 
+//function to get address info
 let getAddressInfo = async () => {
-  //get the user address info
-  let addressInfo = document.getElementById("address").value;
-  //add the amount charged to local storage
   helperFunctions.addToLocalStorageObject(
     "customerOrder",
     "address",
-    addressInfo
+    addressInfo.value
   );
 
   //alert the user that there address has been added
-  if (typeof localStorage.getItem("addressInfo") === "string") {
+  if (localStorage.getItem("customerOrder") != null) {
     alert("Your delivery information has been added");
     //send the user to the menu page
     window.location.replace("menu.html");
@@ -21,18 +22,34 @@ let getAddressInfo = async () => {
     delForm.reset();
   }
 };
+
+//add the address info to address field after login
+let addAddressInfoAfterLogin = () => {
+  //get the customerOrder form local storage
+  let customerOrder =
+    helperFunctions.getObjectFromLocalStorage("customerOrder");
+  //then add the address to the enter address box
+  addressInfo.value = customerOrder.address;
+};
+
+//check if the user is logged in
+let checkUserIsLoggedIn = () => {
+  //if the user logged in checkStatus will return true
+  if (checkStatus) {
+    //call the function to add address
+    addAddressInfoAfterLogin();
+  }
+};
+//call the function to check if the user is logged in
+checkUserIsLoggedIn();
+//
 //function to add the collection option to local storage
 let addCollectionOptionDelPage = () => {
-  //add the collection option copying what will be added
-  // when collection form is submitted
-  let customer = {
-    collection: "yes",
-  };
-  //then add the collection info to local storage
-  window.localStorage.setItem("customer", JSON.stringify(customer));
+  //add the collection option to the customerOrder object
+  helperFunctions.addToLocalStorageObject("customerOrder", "collection", true);
 };
 //add the event listeners
-//for the address
+
 document
   .getElementById("submitBtnDel")
   .addEventListener("click", getAddressInfo);

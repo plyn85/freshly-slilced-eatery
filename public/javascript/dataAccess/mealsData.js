8d8fe6c1ce7cc116d7db777051299f9a690ba240
plyn85 <plyn99@gmail.com>
2021-08-31 19:32:41 +0100

    refactoring mealsData.js page

--- public/javascript/dataAccess/mealsData.js
+++ public/javascript/dataAccess/mealsData.js
@@ -1,28 +1,28 @@
-// import everything from fetchAPI.js
-// This will allow resources to be referenced as api.BASE_URL, etc.
-
+//imports
 import * as api from "./fetchAPI.js";
-
+import {
+  getObjectFromLocalStorage,
+  addObjectToLocalStorage,
+} from "../helper_functions/helpers.js";
 //
-// Get all meals
+//
 let getMeals = async () => {
   try {
-    // get meals data - note only one parameter in function call
+    // get meals data
     return await api.getDataAsync(`${api.BASE_URL}/meals`);
   } catch (err) {
-    // catch and log any errors
     console.log(err);
   }
-}; // End Functions
+};
+// End Function
 
-// Get all meals
 let addMealToCart = async (mealData) => {
   //get the user id if it exists in localStorage
   if (typeof localStorage.getItem("userId") == "string") {
     //if it does add to mealData to be sent it the request body
-    mealData.user_id = JSON.parse(localStorage.getItem("userId"));
+    mealData.user_id = getObjectFromLocalStorage("userId");
   }
-  //http method is post
+
   let httpMethod = "POST";
   //build the request object
   const request = api.fetchInit(httpMethod, JSON.stringify(mealData));
@@ -38,19 +38,15 @@ let addMealToCart = async (mealData) => {
     //if its a sting the id has been sent
     if (typeof responseUserId.user_id == "number") {
       //add the uniqueId to session storage and return true
-      localStorage.setItem("userId", JSON.stringify(responseUserId.user_id));
+      addObjectToLocalStorage("userId", responseUserId.user_id);
     }
     // if the response is false the item is already in the cart
     if (responseUserId == false) {
       alert("That item is already in your cart");
-    }
-    //if the response is either true or a sting an item has been added to the cart
-    else {
+    } else {
       alert("A meal has been added to your cart");
     }
   } catch (err) {
-    //catch the errors
-    // catch and log any errors
     console.log(err);
   }
 }; // End Functions
